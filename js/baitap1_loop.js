d2443528fe5572e5868738e4827c123a9b2bf7a8
LyTrungHai <trunghai1610@gmail.com>
2021-08-21 22:56:31 +0700

    update

--- js/baitap1_loop.js
+++ js/baitap1_loop.js
@@ -14,3 +14,17 @@ function demChanLe() {
   document.getElementById("txtLift").innerHTML =
     "(-) Số chẵn: " + evenNums + "<br> (-)Số lẻ: " + oddNums;
 }
+
+// cach viet while
+// var num = 0;
+// var evenNums = "";
+// var oddNums = "";
+// while (num < 100) {
+//   if (num % 2 == 0) {
+//     evenNums += num + " ";
+//   } else {
+//     oddNums += num + " ";
+//   }
+//   num++;
+// }
+// console.log(evenNums, " \n", oddNums);
--- js/baitap2_loop.js
+++ js/baitap2_loop.js
@@ -9,3 +9,14 @@ function chiaHetCho3() {
   document.getElementById("txtCount").innerHTML =
     "Số chia hết cho 3 nhỏ hơn 1000 : " + count;
 }
+
+//cach viet while
+// var num = 0;
+// var count = 0;
+// while (num < 1000) {
+//   if (num % 3 == 0) {
+//     count++;
+//   }
+//   num++;
+// }
+// console.log(count);
